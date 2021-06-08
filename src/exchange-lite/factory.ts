47f95bcfac410a5aa4269718e97a52ffef41d8c1
infvoid <infvoid.defi@yandex.ru>
2021-06-08 20:01:38 +0800

    feat lite

--- src/exchange-lite/factory.ts
+++ src/exchange-lite/factory.ts
@@ -1,8 +1,11 @@
 /* eslint-disable prefer-const */
 import { BigInt } from "@graphprotocol/graph-ts";
-import { Factory, Pair, Token } from "../../generated/schema";
+import { Pair, Token, Factory } from "../../generated/schema";
+import { Pair as PairTemplate } from "../../generated/templates";
 import { PairCreated } from "../../generated/Factory/Factory";
+import { ZERO_BD } from "./utils";
 import { fetchTokenDecimals, fetchTokenName, fetchTokenSymbol } from "./utils/bep20";
+import { WHITELIST } from "./utils/pricing";
 
 // Constants
 let FACTORY_ADDRESS = "0xca143ce32fe78f1f7019d7d551a6402fc5350c73";
@@ -32,6 +35,8 @@ export function handlePairCreated(event: PairCreated): void {
       return;
     }
     token0.decimals = decimals;
+    token0.derivedBNB = ZERO_BD;
+    token0.whitelist = [];
 
     // Factory
     factory.totalTokens = factory.totalTokens.plus(ONE_BI);
@@ -48,9 +53,25 @@ export function handlePairCreated(event: PairCreated): void {
       return;
     }
     token1.decimals = decimals;
+    token1.derivedBNB = ZERO_BD;
+    token1.whitelist = [];
 
     // Factory
     factory.totalTokens = factory.totalTokens.plus(ONE_BI);
+  }
+
+  // Add (whitelisted) pairs from token0 to token1 entity.
+  if (WHITELIST.includes(token0.id)) {
+    let whitelistedPairs = token1.whitelist;
+    whitelistedPairs.push(event.params.pair.toHex());
+    token1.whitelist = whitelistedPairs;
+  }
+
+  // Add (whitelisted) pairs from token1 to token0 entity.
+  if (WHITELIST.includes(token1.id)) {
+    let whitelistedPairs = token0.whitelist;
+    whitelistedPairs.push(event.params.pair.toHex());
+    token0.whitelist = whitelistedPairs;
   }
 
   // Pair
@@ -58,6 +79,14 @@ export function handlePairCreated(event: PairCreated): void {
   pair.token0 = token0.id;
   pair.token1 = token1.id;
   pair.name = token0.symbol.concat("-").concat(token1.symbol);
+  pair.reserve0 = ZERO_BD;
+  pair.reserve1 = ZERO_BD;
+  pair.reserveBNB = ZERO_BD;
+  pair.token0Price = ZERO_BD;
+  pair.token1Price = ZERO_BD;
+  pair.volumeToken0 = ZERO_BD;
+  pair.volumeToken1 = ZERO_BD;
+  pair.volumeBNB = ZERO_BD;
   pair.block = event.block.number;
   pair.timestamp = event.block.timestamp;
 
@@ -66,4 +95,7 @@ export function handlePairCreated(event: PairCreated): void {
   token1.save();
   pair.save();
   factory.save();
+
+  // Template
+  PairTemplate.create(event.params.pair);
 }
