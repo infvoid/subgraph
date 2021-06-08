c14c63b8479ab5e17655b7b11d8be8ed683ce82e
infvoid <infvoid.defi@yandex.ru>
2021-06-08 19:19:32 +0800

    fix contract address

--- src/pairs/factory.ts
+++ src/pairs/factory.ts
@@ -5,7 +5,7 @@ import { PairCreated } from "../../generated/Factory/Factory";
 import { fetchTokenDecimals, fetchTokenName, fetchTokenSymbol } from "./utils/bep20";
 
 // Constants
-let FACTORY_ADDRESS = "0xca143ce32fe78f1f7019d7d551a6402fc5350c73";
+let FACTORY_ADDRESS = "0xeedcce959675ae3d8974741e80aaa8244a6e3507";
 
 // BigNumber-like references
 let ZERO_BI = BigInt.fromI32(0);
